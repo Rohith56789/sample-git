920a98b8b834c7d0b8bb374b790a82503c29579b
rohith56789 <rohithpenugonda69@gamil.com>
2025-05-17 12:11:47 +0530

    Python Test

---
+++ test_main.py
@@ -0,0 +1,32 @@
+
+class TestMain(unittest.TestCase):
+    import unittest
+from main import add
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        assert add(2,3) == 5
+        assert add(-1,1) == 0
+        
+#         import unittest
+# from main import add, subtract, multiply, divide
+
+    # def test_add(self):
+    #     self.assertEqual(add(1, 2), 3)
+    #     self.assertEqual(add(-1, 1), 0)
+    #     self.assertEqual(add(-1, -1), -2)
+
+    # def test_subtract(self):
+    #     self.assertEqual(subtract(2, 1), 1)
+    #     self.assertEqual(subtract(1, 1), 0)
+    #     self.assertEqual(subtract(-1, -1), 0)
+
+    # def test_multiply(self):
+    #     self.assertEqual(multiply(2, 3), 6)
+    #     self.assertEqual(multiply(-1, 1), -1)
+    #     self.assertEqual(multiply(-1, -1), 1)
+
+    # def test_divide(self):
+    #     self.assertEqual(divide(6, 3), 2)
+    #     with self.assertRaises(ValueError):
+    #         divide(1, 0)
